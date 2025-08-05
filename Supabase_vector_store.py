a0a4881be7b39d0b35d0a8e59aca49b30ef99f74
Selin Kıncal <selinkincal48@gmail.com>
2025-08-05 10:27:26 +0300

    Update Supabase_vector_store.py

--- Supabase_vector_store.py
+++ Supabase_vector_store.py
@@ -5,10 +5,14 @@ from llama_index.vector_stores.supabase import SupabaseVectorStore
 from llama_index.core.schema import TextNode
 from llama_index.core.vector_stores import ExactMatchFilter, MetadataFilters
 import textwrap
+from dotenv import load_dotenv
+load_dotenv() 
 
-os.environ["OPENAI_API_KEY"] = "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"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PG_CONN_STR = os.getenv("PG_CONN_STR")
 
-PG_CONN_STR = "postgresql://postgres:spbsllamaındex2025@db.phberswfcmldavtuircd.supabase.co:5432/postgres"
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
 documents = SimpleDirectoryReader("./data/").load_data()
 print(f"Yüklenen belge ID: {documents[0].doc_id}")
 
@@ -16,7 +20,7 @@ vector_store_docs = SupabaseVectorStore(
     postgres_connection_string=PG_CONN_STR,
     collection_name="base_demo"
 )
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
+storage_context = StorageContext.from_defaults(vector_store=vector_store_docs)
 index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
 
 nodes = [
@@ -29,14 +33,14 @@ vector_store_metadata = SupabaseVectorStore(
     postgres_connection_string=PG_CONN_STR,
     collection_name="metadata_demo"
 )
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
+storage_context = StorageContext.from_defaults(vector_store=vector_store_metadata)
 index = VectorStoreIndex(nodes, storage_context=storage_context)
-
 
 filters = MetadataFilters(filters=[ExactMatchFilter(key="theme", value="Mafia")])
 retriever = index.as_retriever(filters=filters)
 results = retriever.retrieve("What is inception about?")
 for result in results:
     print(result.node.text, result.node.metadata)
+
 
 
